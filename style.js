d74ec96bfb78a1a9eb392f8d7445a478d7065c19
Ismail <42185028+sk-ismail@users.noreply.github.com>
2020-09-01 00:27:19 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,14 @@
+//document.querySelectorAll("#myBtn").onClick = function(){
+  //  document.querySelectorAll("h2").style.color= "blue";
+//};
+
+//function(){
+ //   var a= Math.floor((Math.random()*6)+ 1);
+ //   var b= math.floor((math.random()*6) + 1);
+//    if(a>b){
+ //       document.querySelectorAll(".resulttxt").innerHtml="PLayer A is the Winner";
+ //   }
+ //   else{
+  //      document.querySelectorAll(".resulttxt").innerHtml="PLayer B is the Winner";
+  //  }
+//}
